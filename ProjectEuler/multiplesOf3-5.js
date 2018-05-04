476f7d3fa2b3d7032f7ca084b541d6b038e22a6a
John Long <jplong91@gmail.com>
2018-05-04 08:24:42 -0500

    Euler Problem #1 - sum of multiples 3-5

---
+++ ProjectEuler/multiplesOf3-5.js
@@ -0,0 +1,11 @@
+function sumMultiplesOf3and5() {
+  let sum = 0;
+  for (i = 0; i < 1000; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+console.log(sumMultiplesOf3and5());

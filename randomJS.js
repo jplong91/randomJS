b9577fcb7560ab96cd8f984e8a6c85da9143eb1c
John Long <jplong91@gmail.com>
2018-05-04 08:15:34 -0500

    randomJS

--- randomJS.js
+++ randomJS.js
@@ -1,14 +1,27 @@
-let boy = {hair: "blue"}
-console.log(boy)
+// let boy = {hair: "blue"}
+// console.log(boy)
 
-function removeProperty(obj, prop) {
-  let output = true
-  if (obj[prop]) {
-    delete obj[prop];
-  } else {
-    output = false;
+// function removeProperty(obj, prop) {
+//   let output = true
+//   if (obj[prop]) {
+//     delete obj[prop];
+//   } else {
+//     output = false;
+//   }
+//   return output;
+// };
+
+// console.log(removeProperty(boy, "hair"))
+
+function formatDate(userDate) {
+  // format from M/D/YYYY to YYYYMMDD
+  dayArray = userDate.split("/")
+  output = ""
+  for (i = dayArray.length - 1; i >= 0; i--) {
+    output.concat(dayArray[i]);
+    console.log(output);
   }
   return output;
-};
+}
 
-console.log(removeProperty(boy, "hair"))
+console.log(formatDate("12/31/2014"));
